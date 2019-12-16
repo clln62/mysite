ef404b444452bf2d8bea13acfd52474caeaec819
Collin Fairlie <collin.fairlie@gmail.com>
2019-12-16 17:50:00 -0600

    Added photos for portfolio. Needs Css and possibly video instead.

--- client/src/components/portfolio.jsx
+++ client/src/components/portfolio.jsx
@@ -6,7 +6,10 @@ const Portfolio = (props) => {
     return (
         <div>
             <Navbar />
-            This is where the Portfolio will go.
+            <img className='photo' src="https://collinpersonal.s3.us-east-2.amazonaws.com/IH1.png"></img>
+            <img className='photo' src="https://collinpersonal.s3.us-east-2.amazonaws.com/IH2.png"></img>
+            <img className='photo' src="https://collinpersonal.s3.us-east-2.amazonaws.com/IH3.png"></img>
+            <img className='photo' src="https://collinpersonal.s3.us-east-2.amazonaws.com/IH4.png"></img>
         </div>
     )
 }
